0ad33bfa81840a19eab594a35a7585a851fbb16f
LapisOnTop <yoboieliiii@gmail.com>
2026-06-18 16:28:52 +0800

    fix: hardcode supabase credentials

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://pjupqaoqnfekqcycujat.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqdXBxYW9xbmZla3FjeWN1amF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3NTczODAsImV4cCI6MjA5NzMzMzM4MH0.S5NQDNk4AeLfSNf2oog3pa5l8qa3NR8E731PT3UmZfg";
 
 export const supabase = supabaseUrl && supabaseAnonKey 
   ? createClient(supabaseUrl, supabaseAnonKey) 
